3dcf7d325506aa6cd57f122012f15ca835156965
보경 <97583162+bokoo14@users.noreply.github.com>
2023-12-18 19:04:44 +0900

    set project

---
+++ VoiceMemoApp/VoiceMemoApp/VoiceMemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoAppApp.swift
+//  VoiceMemoApp
+//
+//  Created by Bokyung on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

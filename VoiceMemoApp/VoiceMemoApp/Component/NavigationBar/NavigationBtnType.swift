5bed937a58c88be42da5b5eec1542b5f9bba73db
보경 <qkrqhru@knu.ac.kr>
2023-12-26 21:25:30 +0900

    [Feat] CustomNavigationBar 구현

---
+++ VoiceMemoApp/VoiceMemoApp/Component/NavigationBar/NavigationBtnType.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationBtnType.swift
+//  VoiceMemoApp
+//
+//  Created by Bokyung on 12/21/23.
+//
+
+import Foundation
+
+enum NavigationBtnType: String {
+    case close
+    case edit = "편집"
+    case complete = "완료"
+    case create = "생성"
+}

97b8d43e8fc8a771bf03c0970d7d276b812b2d1f
보경 <qkrqhru@knu.ac.kr>
2023-12-26 21:28:09 +0900

    Create HomeView.swift

---
+++ VoiceMemoApp/VoiceMemoApp/Feature/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  VoiceMemoApp
+//
+//  Created by Bokyung on 12/21/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("HomeView")
+    }
+}
+
+#Preview {
+    HomeView()
+}

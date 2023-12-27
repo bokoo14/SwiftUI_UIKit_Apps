6fd0b173b8f13a9d5434cafdb7153c5b3ab20cc8
보경 <qkrqhru@knu.ac.kr>
2023-12-27 22:18:43 +0900

    [Feat] VoiceRecordView 생성

---
+++ VoiceMemoApp/VoiceMemoApp/Feature/Home/VoceRecorder/VoiceRecorderView.swift
@@ -0,0 +1,32 @@
+//
+//  VoiceRecorderView.swift
+//  VoiceMemoApp
+//
+//  Created by Bokyung on 12/26/23.
+//
+
+import SwiftUI
+
+struct VoiceRecorderView: View {
+    @StateObject private var voiceRecorderViewModel = VoiceRecorderViewModel()
+    
+    var body: some View {
+        ZStack {
+            Text("VoiceRecorderView")
+            // 타이틀 뷰
+            
+            // 안내뷰
+            // 보이스 레코더 리스트 뷰
+            
+            // 녹음버튼 뷰
+        }
+        
+    }
+}
+
+
+
+
+#Preview {
+    VoiceRecorderView()
+}

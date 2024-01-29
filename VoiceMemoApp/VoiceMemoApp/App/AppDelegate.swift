c0da0d99063037fe16642610f8a35a3b500931ea
보경 <qkrqhru@knu.ac.kr>
2024-01-29 21:58:49 +0900

    [Feat] UserNotification 구현

--- VoiceMemoApp/VoiceMemoApp/App/AppDelegate.swift
+++ VoiceMemoApp/VoiceMemoApp/App/AppDelegate.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
-class AppDelegate {
+// 앱에서 일어나는 상호작용, 시스템 로우 레벨에서 할 수 있는 것들을 컨트롤 가능
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var notificationDelegate = NotificationDelegate()
     
+    // 런치가 된 후 수행
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = notificationDelegate
+        return true
+    }
 }
--- VoiceMemoApp/VoiceMemoApp/App/VoiceMemoAppApp.swift
+++ VoiceMemoApp/VoiceMemoApp/App/VoiceMemoAppApp.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 @main
 struct VoiceMemoAppApp: App {
+    // UIKit의 appdelegate를 쓰기 위함
+    // 앱의 선언부에서만 정의, 한번만 선언
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             OnboardingView()

35890b0131d9fe7146dbb35bcffa91b8cfa0ace7
보경 <qkrqhru@knu.ac.kr>
2024-01-30 20:20:31 +0900

    [Feat] TabView생성

--- VoiceMemoApp/VoiceMemoApp/Feature/Home/HomeView.swift
+++ VoiceMemoApp/VoiceMemoApp/Feature/Home/HomeView.swift
@@ -8,11 +8,90 @@
 import SwiftUI
 
 struct HomeView: View {
+    @EnvironmentObject private var pathModel: PathModel
+    @StateObject private var homeViewModel = HomeViewModel()
+    
     var body: some View {
-        Text("HomeView")
+        ZStack {
+            TabView(selection: $homeViewModel.selectedTab) {
+                TodoListView()
+                    .tabItem {
+                        Image(
+                            homeViewModel.selectedTab == .todoList
+                            ? "todoIcon_selected"
+                            : "todoIcon"
+                        ) }
+                    .tag(Tab.todoList)
+                
+                MemoListView()
+                    .tabItem {
+                        Image(
+                            homeViewModel.selectedTab == .memo
+                            ? "memoIcon_selected"
+                            : "memoIcon"
+                        ) }
+                    .tag(Tab.memo)
+                
+                VoiceRecorderView()
+                    .tabItem {
+                        Image(
+                            homeViewModel.selectedTab == .voiceRecorder
+                            ? "recordIcon_selected"
+                            : "recordIcon"
+                        ) }
+                    .tag(Tab.voiceRecorder)
+                
+                TimerView()
+                    .tabItem {
+                        Image(
+                            homeViewModel.selectedTab == .timer
+                            ? "timerIcon_selected"
+                            : "timerIcon"
+                        ) }
+                    .tag(Tab.timer)
+                
+                SettingView()
+                    .tabItem {
+                        Image(
+                            homeViewModel.selectedTab == .setting
+                            ? "settingIcon_selected"
+                            : "settingIcon"
+                        ) }
+                    .tag(Tab.setting)
+            } // TabView
+            // 하위 뷰에서 사용하기 위해 environmentObject로 주입시켜줘야 함
+            .environmentObject(homeViewModel)
+            // 구분선
+            SeperatorLineView()
+        } // ZStack
     }
 }
 
+
+// MARK: 구분선
+private struct SeperatorLineView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Rectangle()
+                .fill(
+                    LinearGradient(
+                        gradient: Gradient(colors: [Color.white, Color.gray.opacity(0.1)]),
+                        startPoint: .top,
+                        endPoint: .bottom
+                    )
+                )
+                .frame(height: 10)
+                .padding(.bottom, 60)
+        } // VStack
+    }
+}
+
+
 #Preview {
     HomeView()
+        .environmentObject(PathModel())
+        .environmentObject(TodoListViewModel())
+        .environmentObject(MemoListViewModel())
 }
---
+++ VoiceMemoApp/VoiceMemoApp/Model/Home/Tab.swift
@@ -0,0 +1,16 @@
+//
+//  Tab.swift
+//  VoiceMemoApp
+//
+//  Created by Bokyung on 1/30/24.
+//
+
+import Foundation
+
+enum Tab {
+    case todoList
+    case memo
+    case voiceRecorder
+    case timer
+    case setting
+}
